8dabb8a1aa58490b57cb6327f40b22a9bbed16ac
Raph <94909118+Ryustiel@users.noreply.github.com>
2022-11-09 23:01:11 +0100

    gunicorn?

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python testrun.py
+web: gunicord wsgi:app
--- main.py
+++ main.py
@@ -5,8 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Mangoes!</h1>"
-
-if __name__ == '__main__':
-    app.run()
-
     
